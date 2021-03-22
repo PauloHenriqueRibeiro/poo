57409334baa15f7232f7dd4eeed8268d1ba8231d
TIC PC <tic@dovale.com.br>
2021-03-22 10:30:23 -0300

    Adicionado uma funcao para calcular a soma de 2 numeros

--- POO/Program.cs
+++ POO/Program.cs
@@ -8,14 +8,15 @@ namespace POO
 
         static void Main(string[] args)
         {
+            int n1, n2, sum;
 
-            TimeSpan t1 = TimeSpan.MaxValue;
-            TimeSpan t2 = TimeSpan.MinValue;
+            Console.WriteLine("Digite um numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outr numero: ");
+            n2 = int.Parse(Console.ReadLine());
+            sum = n1 + n2;
+            Console.WriteLine("A soma de ambos numeros é: " + sum);
 
-            Console.WriteLine("Tenha um bom dia ");
-
-            Console.WriteLine(t1);
-            Console.WriteLine(t2);
 
 
         }
